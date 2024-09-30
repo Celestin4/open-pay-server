1d68dbe9ed253adbbaae8b331d59f181683bb862
Celestin4 <nteziryayocelestin4@gmail.com>
2024-09-30 17:30:42 +0200

    fixing deployment script

--- dist/helpers/generateAccountNumber.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateAccountNumber = void 0;
-const generateAccountNumber = (phoneNumber) => {
-    return '8080' + phoneNumber;
-};
-exports.generateAccountNumber = generateAccountNumber;
--- dist/helpers/generateEmployeeId.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateEmployeeId = generateEmployeeId;
-function generateEmployeeId(prefix) {
-    const currentYear = new Date().getFullYear().toString().slice(-2);
-    const randomDigits = Math.floor(Math.random() * 10000).toString().padStart(4, '0');
-    const employeeId = `${prefix}${currentYear}${randomDigits}`;
-    return employeeId;
-}
